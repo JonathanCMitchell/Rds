93cc0b1d4211876da85dd4bde79f71853fc0a0ee
= <Jmitchell1991@gmail.com>
2016-08-20 16:19:50 -0700

    selectionObj functional search top, rising, hot

--- server/model/search.model.js
+++ server/model/search.model.js
@@ -9,29 +9,62 @@ function searchGet(params) {
 
 
 	console.log('params in side searchModel as object: ', params)
-	var subReddit = {}
-	subReddit.search = params.search
-	subReddit.hot = params.hot
-	subReddit.rising = params.rising;
-	subReddit.top = params.top
 
+
+
+
+
+
+	var subReddit = params.searchTerm
+	var selectionObj = JSON.parse(params.formData)
+
+	console.log('subReddit inside searchGet in search.model.js: ',subReddit);
+	console.log('selectionObj inside searchGet in search.model.js: ', selectionObj);
+	//if rising, use selectionObj.selection.rising
+	//if top, use selectionObj.selection.top
+	//if hot, use selectionObj.selection.hot
 	
-
-if (subReddit[hot] === true) {
-	return new Promise(function(resolve,reject) {
-		request.get('https://www.reddit.com/r/' + subReddit.search + '/' + subReddit.hot + '/' +'.json', function(error,response, body) {
-			if (!error) {
-				// console.log('body inside search model get',body);
-				resolve(body)
-			}
-			  else {
-				reject(error)
-			}
+if (!!subReddit) {	
+	if (selectionObj.selection.rising) {
+		return new Promise(function(resolve,reject) {
+			request.get('https://www.reddit.com/r/' + subReddit + '/' + 'rising' + '/' +'.json', function(error,response, body) {
+				if (!error) {
+					// console.log('body inside search model get',body);
+					resolve(body)
+				}
+				  else {
+					reject(error)
+				}
+				})
 			})
-		})
-}  else {
+	}  else if (selectionObj.selection.hot) {
+		return new Promise(function(resolve,reject) {
+			request.get('https://www.reddit.com/r/' + subReddit + '/' + 'hot' + '/' +'.json', function(error,response, body) {
+				if (!error) {
+					// console.log('body inside search model get',body);
+					resolve(body)
+				}
+				  else {
+					reject(error)
+				}
+				})
+			})
+	}  else if (selectionObj.selection.top){
+		return new Promise(function(resolve,reject) {
+			request.get('https://www.reddit.com/r/' + subReddit + '/' + 'top' + '/' +'.json', function(error,response, body) {
+				if (!error) {
+					// console.log('body inside search model get',body);
+					resolve(body)
+				}
+				  else {
+					reject(error)
+				}
+				})
+			})
+	}
+	else {
 		return new Promise(function(resolve, reject) {
-		request.get('https://www.reddit.com/r/' + subRedditsearch + '.json', function(error,response, body) {
+		request.get('https://www.reddit.com/r/' + subReddit + '.json', function(error,response, body) {
 			if (!error) {
 				// console.log('body inside search model get',body);
 				resolve(body)
@@ -42,5 +75,6 @@ if (subReddit[hot] === true) {
 			})
 		})
 	}
+}
 }
 
