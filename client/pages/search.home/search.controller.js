71082d3bec7e22a66583117dc6ffc6e163af9159
= <Jmitchell1991@gmail.com>
2016-08-20 19:06:14 -0700

    angular navbar try

--- client/pages/search.home/search.controller.js
+++ client/pages/search.home/search.controller.js
@@ -25,21 +25,70 @@
 				})
 			}
 
+		
+
+			 
+
+     
 
 
-			  // angular
-     //  .module('selectDemoBasic', ['ngMaterial'])
-     //  .controller('AppCtrl', function($scope) {
-     //    this.userState = '';
-     //    this.states = ('top rising new hot CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-     //        'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-     //        'WY').split(' ').map(function (state) { return { abbrev: state }; });
-     //  });
+			$scope.items = ['Hot', 'Rising', 'Top'];
+			$scope.selectedItem;
+			$scope.getSelectedText = function() {
+				var term = $scope.selectedItem;
+				if ($scope.search.formData.selection !== undefined) {
+					var selectObj = $scope.search.formData.selection;
+				}  else {
+					console.log('scope.search.formData inside getSelectedItems is undefined', selectObj)
+				}
 
+				function toggleHot(obj) {
+					if (obj === undefined) return
+						else if (obj[hot] === false) {
+							return obj[hot] === true
+						}
+					}
+				function toggleTop(obj) {
+					if (obj === undefined) return
+						else if (obj[top] === false) {
+							return obj[top] === true
+						}
+				}
+				function toggleRising(obj) {
+					if (obj === undefined) return
+							else if (obj[rising] === false) {
+								return obj[rising] === true
+							}
+					}
+					
+				if ($scope.selectedItem !== undefined) {
+					if ($scope.selectedItem === 'Hot') {
+							toggleHot(selectObj)
+					}
+					if ($scope.selectedItem === 'Top') {
+						toggleTop(selectObj)
+					}
+					if ($scope.selectedItem === 'Rising') {
+						toggleRising(selectObj)
+					}
+					return "You have selected: Item " + $scope.selectedItem;
+				}  else {
+								return "Please select an item";
+					 }
+			};
+					
 
 			
 
 			
+			$scope.logSelectBox = function() {
+				console.log("in select box", $scope.selectedItem)
+				// console.log('select box is hit selection', $scope.selection)
+				// console.log('inside select box scope.states: ', $scope.states)
+				
+				// console.log('inside select box scope.selection: ', $scope.selection)
+				//do Get Request again
+			}
 
 
 			
