6954ec6931d4e378510f0b35efe3e1710760b2f3
= <Jmitchell1991@gmail.com>
2016-08-21 12:12:51 -0700

    select box working with searchTerm

--- client/pages/search.home/search.controller.js
+++ client/pages/search.home/search.controller.js
@@ -25,70 +25,102 @@
 				})
 			}
 
-		
+			 $scope.dataa = {
+			 	model: null, //gets bound to whatever is selected
+			 	availableOptions: [
+			 		{id: 'Hot', name: 'Hot'},
+			 		{id: 'Top', name: 'Top'},
+			 		{id: 'Rising', name: 'Rising'}
+			 	]
+			 }
 
-			 
+				$scope.log = function() {
+					console.log('you clicked', $scope.dataa.model)
+					var term = $scope.dataa.model.toLowerCase()
+					console.log('$scope selection is',$scope.search.formData.selection[term])
+					console.log('term is ', term.toLowerCase())
+					console.log('inside log scope search formdata',$scope.search.formData.selection[term])
+					if ($scope.search.formData.selection[term] === false) {
+					$scope.search.formData.selection[term] = true
+					}
+					console.log('scope search formData selection',$scope.search.formData.selection)
+					//then submit the getData request
+					if ($scope.search.searchTerm) {
+					Search.getSearch($scope.search)
+					.then(function(data) {
+						console.log('data in log in scope', data.data.children)
+						$scope.dataArr = data.data.data.children
+					})
+					}  else {
+						console.log('please enter valid search term')
+					}
+				}
+
+
 
      
 
 
-			$scope.items = ['Hot', 'Rising', 'Top'];
-			$scope.selectedItem;
-			$scope.getSelectedText = function() {
-				var term = $scope.selectedItem;
-				if ($scope.search.formData.selection !== undefined) {
-					var selectObj = $scope.search.formData.selection;
-				}  else {
-					console.log('scope.search.formData inside getSelectedItems is undefined', selectObj)
-				}
+			// $scope.items = ['Hot', 'Rising', 'Top'];
+			// $scope.selectedItem;
+			// $scope.getSelectedText = function() {
+			// 	console.log('$scope.selectedItem',$scope.selectedItem)
+			// 	var term = $scope.selectedItem;
+			// 	if ($scope.search.formData.selection !== undefined) {
+			// 		var selectObj = $scope.search.formData.selection;
+			// 	}  else {
+			// 		console.log('scope.search.formData inside getSelectedItems is undefined', selectObj)
+			// 	}
 
-				function toggleHot(obj) {
-					if (obj === undefined) return
-						else if (obj[hot] === false) {
-							return obj[hot] === true
-						}
-					}
-				function toggleTop(obj) {
-					if (obj === undefined) return
-						else if (obj[top] === false) {
-							return obj[top] === true
-						}
-				}
-				function toggleRising(obj) {
-					if (obj === undefined) return
-							else if (obj[rising] === false) {
-								return obj[rising] === true
-							}
-					}
-					
-				if ($scope.selectedItem !== undefined) {
-					if ($scope.selectedItem === 'Hot') {
-							toggleHot(selectObj)
-					}
-					if ($scope.selectedItem === 'Top') {
-						toggleTop(selectObj)
-					}
-					if ($scope.selectedItem === 'Rising') {
-						toggleRising(selectObj)
-					}
-					return "You have selected: Item " + $scope.selectedItem;
-				}  else {
-								return "Please select an item";
-					 }
-			};
+			// 	function toggleHot(obj) {
+			// 		if (obj === undefined) return
+			// 			else if (obj[hot] === false) {
+			// 				console.log('toggleHot', obj)
+			// 				return obj[hot] === true
+			// 			}
+			// 	}
+			// 	function toggleTop(obj) {
+			// 		if (obj === undefined) return
+			// 			else if (obj[top] === false) {
+			// 				console.log('toggleTop', obj)
+			// 				return obj[top] === true
+			// 			}
+			// 	}
+			// 	function toggleRising(obj) {
+			// 		if (obj === undefined) return
+			// 				else if (obj[rising] === false) {
+			// 					console.log('toggleRising', obj)
+			// 					return obj[rising] === true
+			// 				}
+			// 	}
+			// 	if ($scope.selectedItem !== undefined) {
+			// 		if ($scope.selectedItem === 'Hot') {
+			// 				toggleHot(selectObj)
+			// 		}
+			// 		if ($scope.selectedItem === 'Top') {
+			// 			toggleTop(selectObj)
+			// 		}
+			// 		if ($scope.selectedItem === 'Rising') {
+			// 			toggleRising(selectObj)
+			// 		}
+			// 		return "You have selected: Item " + $scope.selectedItem;
+			// 	}  else {
+			// 					return "Please select an item";
+			// 		 }
+			// };
 					
 
 			
 
 			
-			$scope.logSelectBox = function() {
-				console.log("in select box", $scope.selectedItem)
-				// console.log('select box is hit selection', $scope.selection)
-				// console.log('inside select box scope.states: ', $scope.states)
+			// $scope.logSelectBox = function() {
+			// 	console.log("in select box", $scope.selectedItem)
+			// 	// console.log('select box is hit selection', $scope.selection)
+			// 	// console.log('inside select box scope.states: ', $scope.states)
 				
-				// console.log('inside select box scope.selection: ', $scope.selection)
-				//do Get Request again
-			}
+			// 	// console.log('inside select box scope.selection: ', $scope.selection)
+			// 	//do Get Request again
+			// }
 
 
 			
