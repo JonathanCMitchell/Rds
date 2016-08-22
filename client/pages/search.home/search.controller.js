f5f3f3936e766826214611eec29596e7878c879f
= <Jmitchell1991@gmail.com>
2016-08-22 14:29:26 -0700

    added assets folder include redditpng

--- client/pages/search.home/search.controller.js
+++ client/pages/search.home/search.controller.js
@@ -28,7 +28,7 @@
 					console.log('inside hasImage subRedditObj', subRedditObj)
 					console.log('subredditThumbnail', subRedditObj.data.thumbnail)
 					if (!validateURL(subRedditObj.data.thumbnail)) {
-						subRedditObj.data.thumbnail = '../../lib/reddit.png'
+						subRedditObj.data.thumbnail = '../../assets/reddit.png'
 					console.log('inside hasImage subRedditObj[hasImage]', subRedditObj.hasImage)
 					}
 				})
